3b14f68bcd558a75a0523f36b0ec4a2f56fabc61
tanayshah1 <dhasushah@gmail.com>
2023-10-19 23:12:15 -0700

    updated datatable

--- api/stocks.py
+++ api/stocks.py
@@ -48,55 +48,44 @@ api.add_resource(_ReadStockGraph, '/stock_graph/<string:stock_name>')
 
 # Tanay added code
 
-from flask import Blueprint, jsonify, request
-import yfinance as yf
+from flask import Flask, request, jsonify
+import pandas_datareader.data as web
 import pandas as pd
-from flask_restful import Api, Resource
-import plotly.graph_objs as go
-from datetime import datetime
+import yfinance as yf
 
-stock_api = Blueprint('stock_api', __name__, url_prefix='/api/stocks')
-api = Api(stock_api)
+app = Flask(__name)
 
-# Function to get stock graph for a single ticker symbol
-def get_stock_graph(stock_name):
-    end_date = datetime.now()
-    start_date = end_date - pd.Timedelta(days=4856)
+@app.route('/get_stock_data', methods=['POST'])
+def get_stock_data():
+    # Receive a list of ticker symbols from the front end
+    ticker_symbols = request.json.get('ticker_symbols', [])
 
-    df = yf.download(stock_name, start=start_date, end=end_date)
+    all_data = {ticker: web.DataReader(ticker, 'stooq') for ticker in ticker_symbols}
 
-    graph = go.Figure(data=go.Candlestick(x=df.index,
-                                          open=df['Open'],
-                                          high=df['High'],
-                                          low=df['Low'],
-                                          close=df['Close'],
-                                          name=stock_name))
-    graph.update_layout(title=f'{stock_name} Stock Price',
-                        xaxis_title='Date',
-                        yaxis_title='Price')
+    # Extract the 'Adjusted Closing Price' for each symbol
+    price = pd.DataFrame({ticker: data['Close'] for ticker, data in all_data.items()})
 
-    graph_data = graph.to_dict()
-    graph_data['data'][0]['x'] = graph_data['data'][0]['x'].astype(str).tolist()
-    graph_data['data'][0]['open'] = graph_data['data'][0]['open'].tolist()
-    graph_data['data'][0]['high'] = graph_data['data'][0]['high'].tolist()
-    graph_data['data'][0]['low'] = graph_data['data'][0]['low'].tolist()
-    graph_data['data'][0]['close'] = graph_data['data'][0]['close'].tolist()
+    # Convert the DataFrame to a JSON response
+    response = price.to_json(orient='split')
+    
+    return response
 
-    return graph_data
+if __name__ == '__main__':
+    app.run()
 
-# Class for fetching stock data for multiple ticker symbols
-class _ReadStockGraphs(Resource):
-    def post(self):
-        ticker_symbols = request.json.get('ticker_symbols', [])
-        stock_data = {}
-        
-        for ticker_symbol in ticker_symbols:
-            stock_data[ticker_symbol] = get_stock_graph(ticker_symbol)
-        
-        return stock_data
+import requests
 
-# Add the resource for fetching stock data for multiple ticker symbols
-api.add_resource(_ReadStockGraphs, '/stock_graphs')
+# Define the list of ticker symbols to fetch
+ticker_symbols = ['AAPL', 'NVDA', 'MSFT', 'TSLA', 'AMZN', 'NFLX', 'QCOM', 'SBUX']
 
-# Add a resource for fetching stock data for a single ticker symbol (unchanged)
-api.add_resource(_ReadStockGraph, '/stock_graph/<string:stock_name>')
+# Create a dictionary with the ticker symbols
+data = {'ticker_symbols': ticker_symbols}
+
+# Send a POST request to the backend
+response = requests.post('http://your-backend-url/get_stock_data', json=data)
+
+# Extract the JSON response
+stock_data = response.json()
+
+# Process or display the stock data as needed
+print(stock_data)
