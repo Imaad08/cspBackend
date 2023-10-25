d8a932414e2b9d341662d6222290d78714b72028
Imaad08 <imaadmuzaffer@gmail.com>
2023-10-25 09:25:30 -0700

    new functionalities

--- api/stocks.py
+++ api/stocks.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 import pandas as pd
 import yfinance as yf
 import plotly.graph_objs as go
@@ -37,10 +37,40 @@ def get_stock_graph(stock_name):
     return graph_data
 
 
+
 class _ReadStockGraph(Resource):
     def get(self, stock_name):
         graph = get_stock_graph(stock_name)
         return graph
 
-
 api.add_resource(_ReadStockGraph, '/stock_graph/<string:stock_name>')
+
+class _GetStockInfo(Resource):
+    def get(self, stock_name):
+        end_date = datetime.now()
+        start_date = end_date - pd.Timedelta(days=4856)
+        
+        df = yf.download(stock_name, start=start_date, end=end_date)
+
+        stock_info = {
+            "symbol": stock_name,
+            "data": df.to_dict(),  
+        }
+
+        return jsonify(stock_info)
+
+api.add_resource(_GetStockInfo, '/stock_info/<string:stock_name>')
+
+class _UpdateStockData(Resource):
+    def put(self, stock_name):
+        data = request.get_json()  
+        
+        stock = yf.Ticker(stock_name)
+        history = stock.history(period="1d")  
+        updated_data = history.to_dict(orient='records')[0]
+
+        response = {"message": "Stock data updated successfully", "updated_data": updated_data}
+        return jsonify(response)
+
+api.add_resource(_UpdateStockData, '/update_stock/<string:stock_name>')
+
